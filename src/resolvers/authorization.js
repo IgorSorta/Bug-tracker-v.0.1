ce058648bbb36d31c19998503562a6508b80f86a
IgorSorta <igorsorta@gmail.com>
2020-07-23 16:17:07 +0300

    Add (resolvers/authorization.js): isMessageOwner func.

--- src/resolvers/authorization.js
+++ src/resolvers/authorization.js
@@ -5,6 +5,22 @@ const {
     skip
 } = require('graphql-resolvers');
 
-module.exports = (parent, args, {
-    me
-}) => me ? skip : new ForbiddenError('You are not authenticated. Please login.');
+module.exports = {
+    isAuthenticated: (parent, args, {
+        me
+    }) => me ? skip : new ForbiddenError('You are not authenticated. Please login.'),
+    isMessageOwner: async (parent, {
+        id
+    }, {
+        models,
+        me
+    }) => {
+        const message = await models.Message.findByPk(id, {
+            raw: true
+        });
+
+        if (message.userId !== me.id) throw new ForbiddenError('You are not authenticated as owner.');
+
+        return skip;
+    },
+};
