8d92bb7a87609e1c96ecd13e3740e2a0a0f47a2a
IgorSorta <igorsorta@gmail.com>
2020-07-25 14:13:01 +0300

    Add: add customScalarResolver in resolvers/index.js

--- src/resolvers/index.js
+++ src/resolvers/index.js
@@ -1,5 +1,12 @@
 const userResolver = require('./user');
 const messageResolver = require('./message');
 const bugResolver = require('./bug');
+const {
+    GraphQLDateTime
+} = require('graphql-iso-date');
 
-module.exports = [userResolver, messageResolver, bugResolver];
+const customScalarResolver = {
+    Date: GraphQLDateTime
+};
+
+module.exports = [customScalarResolver, userResolver, messageResolver, bugResolver];
