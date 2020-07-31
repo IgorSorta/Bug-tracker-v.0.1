773331c6411f0133250462ae9b961c39cf597621
IgorSorta <igorsorta@gmail.com>
2020-07-31 16:52:16 +0300

    Update 1-1_user_query.test code

---
+++ test/1-1_user_query.test.js
@@ -0,0 +1,92 @@
+const {
+    models
+} = require('../src/models/index');
+const createToken = require('../src/helpers/createToken');
+const {
+    getQuery
+} = require('./helpers/test_Helpers');
+
+describe('Testing User: query (users, user(id), me)', () => {
+    var TEST_USER;
+    var token;
+    beforeAll(async () => {
+        TEST_USER = await models.User.findOne({
+            where: {
+                name: 'testUser'
+            }
+        });
+
+        token = await createToken(TEST_USER, process.env.SECRET, '30m')
+    }, 10000);
+
+    test('users: should pass if the query is valid', async () => {
+        const test_query = await getQuery({
+            query: `query {
+            users {
+              name
+            }
+          }
+      `
+        });
+
+        // const expectedResult = {
+        //     "data": {
+        //         "users": [{
+        //                 "name": "testUser"
+        //             },
+        //             {
+        //                 "name": "max"
+        //             }
+        //         ]
+        //     }
+        // };
+
+        expect(test_query).not.toBeNull();
+        expect(test_query.status).toBe(200);
+        expect(test_query.data).toBeDefined();
+    });
+
+    test('user(id): should pass if the query is valid', async () => {
+
+        const test_query = await getQuery({
+            query: `query {
+            user(id: "${TEST_USER.id}") {
+              name
+            }
+          }
+      `
+        });
+
+        const expectedResult = {
+            data: {
+                user: {
+                    name: TEST_USER.name,
+
+                },
+            },
+        };
+
+        expect(test_query.data).toMatchObject(expectedResult);
+    });
+
+    test('me: should pass if the query is valid', async () => {
+        const test_query = await getQuery({
+            query: `query {
+                me {
+                    id
+                    name
+                  }
+            }`
+        }, token);
+
+        const expectedResult = {
+            "data": {
+                "me": {
+                    "name": TEST_USER.name
+                }
+            }
+        };
+
+        expect(test_query.data).toMatchObject(expectedResult)
+    })
+});
